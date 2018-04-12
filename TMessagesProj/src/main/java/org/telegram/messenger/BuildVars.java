ae939965264d04f309b6e1789f8fe8d812bacc02
jiulu <zhanghongjun228@163.com>
2018-04-12 12:12:30 +0800

    添加基于4.6源码的编译版本

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,26 @@
+/*
+ * This is the source code of Telegram for Android v. 4.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2017.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static int BUILD_VERSION = 1155;
+    public static String BUILD_VERSION_STRING = "4.6";
+    public static int APP_ID = 226539; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "f0ff3e06e48258b5158132f8599fe34b"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "1b921d0864b64f999be0762ef552ed1a";
+    public static String HOCKEY_APP_HASH_DEBUG = "1b921d0864b64f999be0762ef552ed1a";
+    public static String BING_SEARCH_KEY = "f3bdcd8c-7111-4ae2-8314-36c7a9a16cee"; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String GOOGLE_API_KEY = "";
+
+    public static String FOURSQUARE_API_KEY = "FTS0GMSSSKSDAYQZKXZFMNX5QLWKMTEQEIGHWYHMZ3GJCMV3"; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = "AMBP4DKLAWHUYHQ52F5TXPVNJVLGGOFDIB4X23MYJGZCZIWL"; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20150326";
+}
